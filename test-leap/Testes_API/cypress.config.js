400f82619ce62717534c097351c8eeea8a5601d9
João Samuel Vilas Boas Góes <43974970+JoaoSamuel11@users.noreply.github.com>
2025-08-08 14:00:18 -0300

    Update cypress.config.js

--- test-leap/Testes_API/cypress.config.js
+++ test-leap/Testes_API/cypress.config.js
@@ -5,6 +5,7 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
-    baseUrl: 'https://api.chatbot.homolog.loomi.com.br/'
+    baseUrl: ''
   },
 });
+
